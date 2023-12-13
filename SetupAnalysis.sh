9b1b427ad258c769d4561fba2c124f27688c525e
scodella <luca.scodellaro@cern.ch>
2023-12-13 20:14:33 +0100

    EL9

--- SetupAnalysis.sh
+++ SetupAnalysis.sh
@@ -30,8 +30,18 @@ fi
 source $CMSSW_BASE/src/LatinosSetup/Functions.sh
 
 if [[ "$CMSSW_VERSION" == CMSSW_10_*_* ]]; then
+    runSetup='13'
+    multidrawrepo='yiiyama'
+elif [[ "$CMSSW_VERSION" == CMSSW_13_*_* ]]; then
+    runSetup='13.6'
+    multidrawrepo='scodella'
+else
+    echo "Uknown setup for " $CMSSW_VERSION
+    exit 1
+
+if [[ "$CMSSW_VERSION" == CMSSW_*_*_* ]]; then
     echo "======================================="
-    echo "running with $CMSSW_VERSION - this is a 13 TeV setup!"
+    echo "running with $CMSSW_VERSION - this is a " $runSetup " TeV setup!"
     echo "Current time:" $(date)
     echo "checking out additional repositories; this could take a while ..."
     echo "======================================="
@@ -46,9 +56,7 @@ if [[ "$CMSSW_VERSION" == CMSSW_10_*_* ]]; then
     elif [ $usebase == 1 ]; then
         git checkout base
     elif [ $isnew == 1 ]; then
-        echo here
         git checkout base
-        ls
         git checkout -b $analysis base
         sed -i "s|base|${analysis}|g;s|master|base|g" sync2master.sh 
         git mv sync2master.sh sync2base.sh ; git commit -m "sync2master to sync2base script"
@@ -63,41 +71,48 @@ if [[ "$CMSSW_VERSION" == CMSSW_10_*_* ]]; then
 
     echo " - Plots Configurations"
 
-    git clone https://github.com/scodella/PlotsConfigurations PlotsConfigurations
-    cd PlotsConfigurations
     if [ $isnew == 1 ]; then
-        git checkout base
+        git clone -b base git@github.com:scodella/PlotsConfigurations PlotsConfigurations
+        cd PlotsConfigurations
         git checkout -b $analysis base
         sed -i "s|RPLME_ANALYSIS|${analysis}|g" sync2base.sh
         git add sync2base.sh ; git commit -m "update to sync2base script"
+	cd -
     else
-        git checkout $analysis
+        git clone -b $analysis git@github.com:scodella/PlotsConfigurations PlotsConfigurations
     fi
-    cd -
 
     if [ $analysis == 'base' ]; then
         exit 1
     fi
 
-    echo " - Nano Tools"
-
-    git clone https://github.com/scodella/nanoAOD-tools PhysicsTools/NanoAODTools
-
     echo " - Plotting Tools"
 
-    git clone git@github.com:yiiyama/multidraw.git LatinoAnalysis/MultiDraw
+    git clone git@github.com:$multidrawrepo/multidraw.git LatinoAnalysis/MultiDraw
     cd LatinoAnalysis/MultiDraw
     #git checkout 2.0.12 2>/dev/null # This gives me an error
     git checkout 2.0.12 >/dev/null
     ./mkLinkDef.py --cmssw
     cd ../..
 
-    echo " - MELA new version"
+    if [ $analysis == 'worker' ]; then
+      
+	echo " - Nano Tools"
 
-    git clone git@github.com:MELALabs/MelaAnalytics.git MelaAnalytics
-    cd MelaAnalytics ; git checkout -b from-v22 v2.2 ; cd ..
-    git clone https://github.com/JHUGen/JHUGenMela.git JHUGenMELA
-    cd JHUGenMELA; git checkout -b from-v235 v2.3.5 ; source setup.sh -j 12 ; cd ..
+        git clone git@github.com:scodella/nanoAOD-tools PhysicsTools/NanoAODTools
+
+    fi
+
+    if [ $analysis == 'XXX' ]; then
+
+        echo " - MELA new version"
+    
+        git clone git@github.com:MELALabs/MelaAnalytics.git MelaAnalytics
+        cd MelaAnalytics ; git checkout -b from-v22 v2.2 ; cd ..
+        git clone https://github.com/JHUGen/JHUGenMela.git JHUGenMELA
+        cd JHUGenMELA; git checkout -b from-v235 v2.3.5 ; source setup.sh -j 12 ; cd ..
+
+    fi
 
     scram b -j 8
 
